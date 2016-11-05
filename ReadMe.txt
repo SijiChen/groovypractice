843eaae7ba5c5bdaf5f05f506dd0b1f0186f1ff2
SijiChen <chensiji19891@gmail.com>
2016-11-05 08:31:56 -0400

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+Practice of groovy: first time
+following http://www.tutorialspoint.com/groovy/
---
+++ src/main/groovy/Example.groovy
@@ -0,0 +1,11 @@
+/**
+ * Created by sjchen on 11/5/16.
+ */
+class Example {
+
+    void main(String[] args) {
+        // Using a simple println statement to print output to the console
+        println('Hello World');
+    }
+
+}
